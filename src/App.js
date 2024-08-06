c90b3efd3b54cf4866c032332ccb6ce40a4d453a
iculmination <ovsiichuk.bohdan@gmail.com>
2024-08-06 19:26:04 +0300

    components + redux store + currencies

--- src/App.js
+++ src/App.js
@@ -1,9 +1,50 @@
 import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import { Layout, Typography, Space } from "antd";
+
+import {
+  Navbar,
+  Exchanges,
+  Homepage,
+  Cryptocurrencies,
+  News,
+  CryptoDetails,
+} from "./components";
+import "./App.css";
 
 const App = () => {
-  return <div>
-    <h1>Crypto App</h1>
-  </div>;
+  return (
+    <div className="app">
+      <div className="navbar">
+        <Navbar />
+      </div>
+
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Routes>
+              <Route path="/" element={<Homepage />} />
+              <Route path="/exchanges" element={<Exchanges />} />
+              <Route path="/cryptocurrencies" element={<Cryptocurrencies />} />
+              <Route path="/crypto/:coinId" element={<CryptoDetails />} />
+              <Route exact path="/news" element={<News />} />
+            </Routes>
+          </div>
+        </Layout>
+        <footer className="footer">
+          <Typography.Title level={5} style={{ color: "white" }}>
+            Crypto App <br />
+            All rights reserved
+          </Typography.Title>
+          <Space>
+            <Link to="/">Home</Link>
+            <Link to="/exchanges">Exchanges</Link>
+            <Link to="/news">News</Link>
+          </Space>
+        </footer>
+      </div>
+    </div>
+  );
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
+import { createRoot } from "react-dom/client";
+
 import React from "react";
-import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+
+import store from "./app/store";
 
 import App from "./App";
-
-ReactDOM.render(<App />, document.getElementById("root"));
+import "antd/dist/reset.css";
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>
+);
